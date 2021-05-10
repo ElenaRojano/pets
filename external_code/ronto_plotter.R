8ae4d90f3d620d4eb5ff66a18f8f097265a00807
JoseCorCab <josecordoba@uma.es>
2021-05-10 13:44:37 +0200

    circular level plot performance improved

--- external_code/ronto_plotter.R
+++ external_code/ronto_plotter.R
@@ -1,212 +1,138 @@
 #!/usr/bin/env Rscript
-
-#' @author Fernando Moreno Jabato <jabato(at)uma(dot)es>
-
-############################################################
-##                      SETUP PROGRAM                     ##
-############################################################
-# Loading libraries
-suppressPackageStartupMessages(require(optparse)) 
-suppressPackageStartupMessages(require(ggplot2)) 
-suppressPackageStartupMessages(require(ontologyIndex)) 
-suppressPackageStartupMessages(require(pbapply)) 
-
-# Prepare command line input 
-option_list <- list(
-  make_option(c("-i", "--input_file"), type="character", default=NULL,
-    help="Input file to be loaded. Format must be: 1) Term, 2) Value [color], 3) (OPTIONAL) Second value [size]"),
-  make_option(c("-o", "--output_file"), type="character", default=NULL,
-    help="Output graph file name"),
-  make_option(c("-O", "--ontology"), type="character", default=NULL,
-    help="Ontology OBO file to be loaded"),
-  make_option(c("-w", "--white_list"), type="character", default=NULL,
-    help="White list file of terms to be plotted (plus input file terms). If not specified, all ontology terms will be plotted"),
-  make_option(c("-H", "--header"), type="logical", default=FALSE, action = "store_true",
-    help="Flag to indicate that input file has header"),
-  make_option(c("-e", "--expand_wl"), type="logical", default=FALSE, action = "store_true",
-    help="Flag to indicate that white list must be expanded using input terms and their parentals"),
-  make_option(c("-v", "--verbose"), type="logical", default=FALSE, action = "store_true",
-    help="Activate verbose mode")
-)
-opt <- parse_args(OptionParser(option_list=option_list))
-
+#' @author SysBioLab
 
 ############################################################
 ##                        FUNCTIONS                       ##
 ############################################################
 
-sparseLevelItems <- function(levels){
-  ulvls <- unique(levels)
-  base_radians <- rep(0,length(levels))
-  invisible(lapply(ulvls,function(lvl){
-    items <- which(levels == lvl)
-    values = seq(length(items)) * ((2*pi)/length(items))
-    base_radians[items] <<- values 
-  }))
+sparseLevelItems <- function(term_levels){
+  u_levels <- unique(term_levels)
+  base_radians <- rep(0,length(term_levels))
+  for (level in u_levels) {
+    terms_in_level <- which(term_levels == level)
+    radial_pos <- seq(length(terms_in_level)) * 
+                   ((2*pi)/length(terms_in_level))
+    base_radians[terms_in_level] <- radial_pos 
+  }
   return(base_radians)
 }
 
-
-calc_paths <- function(ontology, ids = NULL){
-  # Initialize
-  visited_terms <- c()
-  paths <- list()
-  if(is.null(ids)){
-    ids <- as.vector(ontology$id)
-  }else{
-    ids <- as.vector(ids)
+prepare_data <- function(profile_terms, onto, white_list, expand_wl){
+  profile_terms$Type <- "Profile"
+  ontTerms2plot <- onto$id[!onto$obsolete]
+  if(!is.null(white_list)){ # Populate with white list
+    whitelist <- read.table(file = white_list, sep = "\t", stringsAsFactors = FALSE, header = FALSE)[,1]
+    if(expand_wl){
+      whitelist <- unique(c(whitelist, profile_terms[[1]]))
+    }
+    ontTerms2plot <- ontologyIndex::get_ancestors(onto, whitelist)
   }
-  env <- environment()
-  for(id in ids){
-    if(!id %in% env$visited_terms){ # Only if it's not visited
-      paths[[id]] <- list(ID = id, NPaths = 0, ShortestPath = 0, Paths = list())
+  ref_terms <- setdiff(ontTerms2plot, unique(profile_terms[[1]]))
+  ref_terms <- data.frame(Term= ref_terms)
+  ref_terms[,1 + seq(ncol(profile_terms) - 2)] <- 0
+  ref_terms$Type <- "Reference"
+  colnames(ref_terms) <- colnames(profile_terms)
+  terms_info <- rbind(profile_terms, ref_terms)
+  return(terms_info)
+}
+
+calc_all_paths <- function(ids, ontology){
+    env <- environment()
+    paths <- list()
+    calc_path(ids = ids, ontology = ontology, env = env)
+    return(paths)
+}
+
+calc_path <- function(ids, ontology, env){
+  for (id in ids) {
+    if (is.null(env$paths[[id]])) {
+      env$paths[[id]] <- list()
       direct_ancestors <- ontology$parents[[id]]
-      if(length(direct_ancestors) <= 0){
-        paths[[id]]$Paths <- append(paths[[id]]$Paths, list(c(id)))
-      }else{
-        invisible(lapply(direct_ancestors,function(anc){
-          if(!anc %in% env$visited_terms){
-            calc_path(anc,ontology,env)
-          }
-          paths[[id]]$Paths <<- append(paths[[id]]$Paths, lapply(paths[[anc]]$Paths,function(path){c(id,unlist(path))}))
-          env$visited_terms <- c(env$visited_terms,anc)
-        }))
-      }
-      env$visited_terms <- c(env$visited_terms,id)
-    }
-    # Calc metadata
-    paths[[id]]$NPaths <- length(paths[[id]]$Paths)
-    paths[[id]]$ShortestPath <- min(unlist(lapply(paths[[id]]$Paths,function(path){length(unlist(path))})))
-  }
-  return(paths)
-}
-
-
-calc_path <- function(id, ontology, env){
-  if(!id %in% env$visited_terms){
-    env$paths[[id]] <- list(ID = id, NPaths = 0, ShortestPath = 0, Paths = list())
-    direct_ancestors <- ontology$parents[[id]]
-    if(length(direct_ancestors) <= 0){
-      env$paths[[id]]$Paths <- append(env$paths[[id]]$Paths, list(c(id)))
-    }else{
-      invisible(lapply(direct_ancestors,function(anc){
-        if(!anc %in% env$visited_terms){
-          calc_path(anc,ontology,env)
+      if (length(direct_ancestors) == 0) {
+        env$paths[[id]] <- c(env$paths[[id]], list(c(id)))
+      } else {
+        for (anc in direct_ancestors) {
+          calc_path(ids = anc, ontology = ontology,env = env)
+          env$paths[[id]] <- c(env$paths[[id]], 
+                               lapply(env$paths[[anc]], function(path){
+                                c(id,unlist(path))}))
         }
-        env$paths[[id]]$Paths <- append(env$paths[[id]]$Paths, lapply(env$paths[[anc]]$Paths,function(path){c(id,unlist(path))}))
-        env$visited_terms <- c(env$visited_terms,anc)
-      }))
+      }
     }
-    env$visited_terms <- c(env$visited_terms,id)
   }
 }
+
+
+############################################################
+##                    OPTIONS PARSING                     ##
+############################################################
+
+option_list <- list(
+  optparse::make_option(c("-i", "--input_file"), type="character", default=NULL,
+    help="Input file to be loaded. Format must be: 1) Term, 2) Value [color], 3) (OPTIONAL) Second value [size]"),
+  optparse::make_option(c("-o", "--output_file"), type="character", default=NULL,
+    help="Output graph file name"),
+  optparse::make_option(c("-O", "--ontology"), type="character", default=NULL,
+    help="Ontology OBO file to be loaded"),
+  optparse::make_option(c("-w", "--white_list"), type="character", default=NULL,
+    help="White list file of terms to be plotted (plus input file terms). If not specified, all ontology terms will be plotted"),
+  optparse::make_option(c("-H", "--header"), type="logical", default=FALSE, action = "store_true",
+    help="Flag to indicate that input file has header"),
+  optparse::make_option(c("-e", "--expand_wl"), type="logical", default=FALSE, action = "store_true",
+    help="Flag to indicate that white list must be expanded using input terms and their parentals"),
+  optparse::make_option(c("-v", "--verbose"), type="logical", default=FALSE, action = "store_true",
+    help="Activate verbose mode")
+)
+opt <- optparse::parse_args(optparse::OptionParser(option_list=option_list))
 
 ############################################################
 ##                           MAIN                         ##
 ############################################################
 
-apply_fun <- lapply
-if(opt$verbose){ # Verbose point
-  message("Loading input file")
-  apply_fun <- pblapply
-  pboptions(type="txt")
-  ltimes <- list()
-  ltimes$init <- Sys.time()
-} 
+time_ref <- Sys.time()
 
 # Load data
-df <- read.table(file = opt$input_file, sep = "\t", stringsAsFactors = FALSE, header = opt$header) 
-cnames <- c("Term","Color","Size")
-colnames(df) <- cnames[1:ncol(df)]
-
-if(opt$verbose){
-  ltimes$load_init <- Sys.time()
-  message("Loading ontology ...") # Verbose point
-}
+profile_terms <- read.table(file = opt$input_file, sep = "\t", stringsAsFactors = FALSE, header = opt$header) 
+if (!opt$header){
+  cnames <- c("Term","Color","Size")
+  colnames(profile_terms) <- cnames[1:ncol(profile_terms)]
+} 
 
 # Load ontology
-onto <- get_ontology(file = opt$ontology)
-
-if(opt$verbose){
-  ltimes$populate_init <- Sys.time()
-  message(paste0("\tElapsed : ",round(as.numeric(ltimes$populate_init) - as.numeric(ltimes$load_init),2)," s"))
-  message("Populating with all available terms ...") # Verbose point
-}
+onto <- ontologyIndex::get_ontology(file = opt$ontology)
 
 # Populate input df with all allowed terms
-df$Type <- rep("Raw",nrow(df))
-topopulate <- onto$id[!onto$obsolete]
-if(!is.null(opt$white_list)){ # Populate with white list
-  wl <- read.table(file = opt$white_list, sep = "\t", stringsAsFactors = FALSE, header = FALSE)[,1]
-  if(opt$expand_wl) wl <- unique(c(wl,df$Term))
-  topopulate <- unique(unlist(lapply(wl,function(item){get_ancestors(onto,item)})))
-}
 
-aux_df <- as.data.frame(do.call(rbind,apply_fun(setdiff(topopulate,unique(df$Term)),function(id){
-  return(t(as.data.frame(c(id, rep(0, ncol(df)-2), "Added"))))
-})))
-rownames(aux_df) <- NULL
-colnames(aux_df) <- colnames(df)
-df <- rbind(df, aux_df)
+terms_info <- prepare_data(profile_terms, onto, white_list = opt$white_list, expand_wl = opt$expand_wl)
 
-if(opt$verbose){
-  ltimes$levels_init <- Sys.time()
-  message(paste0("\tElapsed : ",round(as.numeric(ltimes$levels_init) - as.numeric(ltimes$populate_init),2)," s"))
-  message("Calculating levels ...") # Verbose point
-}
-
-# Prepare terms
-uniqterms <- unique(df$Term)
-aux_anc <- unlist(lapply(uniqterms,function(term){length(onto$ancestors[[term]])}))
-names(aux_anc) <- uniqterms
-aux_anc <- names(sort(aux_anc, decreasing = T))
 # Obtain levels
-paths <- calc_paths(onto, uniqterms)
-levels <- lapply(paths,function(path){path$ShortestPath})
+paths <- calc_all_paths(ids = terms_info[[1]], ontology = onto)
+levels <- lapply(paths, function(id){min(lengths(id))})
 
-# Sort by code to be (always) the same
-df <- df[order(df$Term),]
+# Sort by code 
+terms_info <- terms_info[order(terms_info[[1]]),]
 
 # Include plotting data
-df$Level <- unlist(lapply(df$Term,function(term){levels[term]}))
-df$Radians <- sparseLevelItems(df$Level)
-df$LevelD <- factor(as.character(df$Level), levels = as.character(c(0,seq(max(df$Level)))))
-df$Color <- as.numeric(df$Color)
+terms_info$Level <- unlist(lapply(terms_info[[1]],function(term){levels[term]}))
+terms_info$Radians <- sparseLevelItems(terms_info$Level)
+terms_info$LevelD <- factor(as.character(terms_info$Level), levels = as.character(c(0,seq(max(terms_info$Level)))))
 
-if(opt$verbose){
-  ltimes$plot_init <- Sys.time()
-  message(paste0("\tElapsed : ",round(as.numeric(ltimes$plot_init) - as.numeric(ltimes$levels_init),2)," s"))
-  message("Generating plot") # Verbose point
+aes_profiles <- ggplot2::aes_string(color = colnames(terms_info)[2])
+if(length(colnames(terms_info)) == 4){
+  aes_profiles <- c(aes_profiles, ggplot2::aes_string(size = colnames(terms_info)[3]))
 }
 
-if("Size" %in% colnames(df)){
-  aes_aux <- aes(x = LevelD, y = Radians, color = Color, size = Size)
-}else{
-  aes_aux <- aes(x = LevelD, y = Radians, color = Color)
-}
+pp <- ggplot2::ggplot(mapping = ggplot2::aes(x = LevelD, y = Radians)) + 
+      ggplot2::geom_point(data = terms_info[terms_info$Type == "Reference",], alpha = 0.3, size = 0.8) + 
+      ggplot2::geom_point(data = terms_info[terms_info$Type == "Profile",],mapping = aes_profiles, alpha = 0.7) + 
+      ggplot2::scale_colour_gradient(low = "orange", high = "red") + 
+      ggplot2::scale_x_discrete("LevelD") + 
+      ggplot2::ylim(c(0,2*pi)) +
+      ggplot2::coord_polar(theta = "y")
 
-pp = ggplot() + 
-      geom_point(data = df[df$Type == "Added",],mapping = aes(x = LevelD, y = Radians), alpha = 0.3, size = 0.8) + 
-      geom_point(data = df[df$Type == "Raw",],mapping = aes_aux, alpha = 0.7) + 
-      scale_colour_gradient(low = "orange", high = "red") + 
-      scale_x_discrete("LevelD") + 
-      ylim(c(0,2*pi)) +
-      coord_polar(theta = "y")
 
-if(opt$verbose){
-  ltimes$render_plot <- Sys.time()
-  message(paste0("\tElapsed : ",round(as.numeric(ltimes$render_plot) - as.numeric(ltimes$plot_init),2)," s"))
-  message("Rendering plot ...") # Verbose point
-}
-
-siz <- 1000
-png(paste0(opt$output_file,".png"), height = siz, width = siz)
-plot(pp)
+png(paste0(opt$output_file,".png"), height = 1000, width = 1000)
+  plot(pp)
 dev.off()
 
-if(opt$verbose){
-  ltimes$end <- Sys.time()
-  message(paste0("\tElapsed : ",round(as.numeric(ltimes$end) - as.numeric(ltimes$render_plot),2)," s"))
-  message(paste0("Program finished. Elapsed : ",round(as.numeric(ltimes$end) - as.numeric(ltimes$init),2)," s"))
-}
 
