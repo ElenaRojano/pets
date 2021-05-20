1842efb89a4b4078a9f0bd9351bce25d7394e4a8
James Richard Perkins <jimrperkins@gmail.com>
2021-05-20 17:37:02 +0200

    optparse added to scatterplot script

--- external_code/plot_scatterplot_simple.R
+++ external_code/plot_scatterplot_simple.R
@@ -1,22 +1,44 @@
 #! /usr/bin/env Rscript
 
+library(optparse)
+
+option_list <- list(
+  optparse::make_option(c("-i", "--info"), type="character", default=NULL,
+                        help="File - needs minimum 2 columns"),
+  optparse::make_option(c("-o", "--output"), type="character", default="out.pdf",
+                        help="column name for x-axis values."),
+  optparse::make_option(c("-x", "--x_axis"), type="character", default=NULL,
+                        help="column name for y-axis values."),
+  optparse::make_option(c("-y", "--y_axis"), type="character", default=NULL,
+                        help="column to use for y-axis values"),
+  optparse::make_option("--x_tag", type="character", default="x-axis",
+                        help="x-axis label"),
+  optparse::make_option("--y_tag", type="character", default="y-axis",
+                        help="y-axis label"),
+  optparse::make_option("--x_lim", type="character", default="NA,NA",
+                        help="x-axis limits (2 values, comma separated, NAs mean axes follow data)"),
+  optparse::make_option("--y_lim", type="character", default="NA,NA",
+                        help="y-axis limits (2 values, comma separated, NAs mean axes follow data)")
+)
+opt <- optparse::parse_args(optparse::OptionParser(option_list=option_list))
+
 library(ggplot2)
-args <- commandArgs(trailingOnly = TRUE)
 
-info <- read.table(args[1], header=TRUE)
-output <- args[2]
-x_axis <- args[3]
-y_axis <- args[4]
-x_tag <- args[5]
-y_tag <- args[6]
+info <- read.table(opt$info, header=TRUE)
+x_lim <- as.numeric(strsplit(opt$x_lim, ",")[[1]])
+y_lim <- as.numeric(strsplit(opt$y_lim, ",")[[1]])
+x_lim_min <- x_lim[[1]]
+x_lim_max <- x_lim[[2]]
+y_lim_min <- y_lim[[1]]
+y_lim_max <- y_lim[[2]]
 
-pdf(file.path(output))
-	p <- ggplot(info, aes(x=info[[x_axis]], y=info[[y_axis]])) + 
+pdf(file.path(opt$output))
+	p <- ggplot(info, aes(x=.data[[opt$x_axis]], y=.data[[opt$y_axis]])) + 
 		geom_point(alpha = 1/10) +
-		xlim(0, NA) +
-		ylim(0, NA) +
-		xlab(x_tag) +
-		ylab(y_tag) +
+		xlim(x_lim_min, x_lim_max) +
+		ylim(y_lim_min, y_lim_max) +
+		xlab(opt$x_tag) +
+		ylab(opt$y_tag) +
 		theme(axis.text.x = element_text(angle = 45, hjust = 1))
 	ggExtra::ggMarginal(
 	  p,
