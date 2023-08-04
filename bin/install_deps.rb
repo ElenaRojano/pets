2cdc16fde341b8f495db8f97d4a7aafcc02d2994
seoanezonjic <darklordsone@hotmail.com>
2023-08-04 10:56:07 +0200

    Fix installation script

--- bin/install_deps.rb
+++ bin/install_deps.rb
@@ -1,7 +1,8 @@
 #! /usr/bin/env ruby
 
 ROOT_PATH = File.dirname(__FILE__)
-EXTERNAL_CODE = File.expand_path(File.join(ROOT_PATH, '..', 'external_code')
+EXTERNAL_CODE = File.expand_path(File.join(ROOT_PATH, '..', 'external_code'))
 $: << File.expand_path(File.join(ROOT_PATH, '..', 'lib', 'pets'))
+require 'pets'
 
-system_call(EXTERNAL_CODE, 'install_R_dependencies.R')
+system_call(EXTERNAL_CODE, 'install_R_dependencies.R', '')
--- external_code/install_R_dependencies.R
+++ external_code/install_R_dependencies.R
@@ -7,5 +7,10 @@ print("Installing libraries from CRAN")
 packages_list <-c("optparse","RcppCNPy","ggplot2","fastcluster","dplyr","gplots","RColorBrewer","tidyr","data.table","gridExtra", "dynamicTreeCut", "ggExtra", "ontologyIndex", "magrittr")
 installed <- library()$results[,1]
 packages_list <- setdiff(packages_list, installed)
-install.packages(packages_list, repos='https://cloud.r-project.org')
+if(length(packages_list) == 0){
+	print('All needed packages are installed')
+}else{
+	install.packages(packages_list, repos='https://cloud.r-project.org')
+}
+
 
